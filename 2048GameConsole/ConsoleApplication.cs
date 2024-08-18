849fd60ae7a6b7e8c70aaf59837b6ecd320b9f9a
VintorezzZ <sergeevmisha16@gmail.com>
2024-08-18 10:47:31 +0300

    Some old forgotten code

--- 2048GameConsole/ConsoleApplication.cs
+++ 2048GameConsole/ConsoleApplication.cs
@@ -1,11 +1,37 @@
 ﻿using _2048GameConsole.Ui;
 using Game2048;
+using Game2048.GameModel;
 using Game2048.InputSystem;
 
 namespace _2048GameConsole;
 
 public class ConsoleApplication : BaseApplication
 {
+    private const int BOARD_SIZE = 4;
+    private Model _game;
+
+    public override void Run()
+    {
+        CreateGame();
+        
+        base.Run();
+        
+        _game.Start();
+    }
+    
+    private void CreateGame()
+    {
+        _game = new Model();
+        _game.Init(BOARD_SIZE);
+    }
+
+    protected override void AddInputHandlers()
+    {
+        _inputTree.Add(_game);
+        
+        base.AddInputHandlers();
+    }
+    
     protected override void CreateScreenManager()
     {
         _screenManager = new ConsoleScreenManager(_uiInputRoot, _game, new ConsoleGameFieldViewUpdater(_game.Grid));
--- Game2048/BaseApplication.cs
+++ Game2048/BaseApplication.cs
@@ -1,14 +1,10 @@
-﻿using Game2048.GameModel;
-using Game2048.InputSystem;
+﻿using Game2048.InputSystem;
 using Game2048.Ui;
 
 namespace Game2048;
 
 public abstract class BaseApplication
 {
-    private const int BOARD_SIZE = 4;
-
-    protected Model _game;
     protected InputManager _inputManager;
     protected UiInputRoot _uiInputRoot;
     protected InputTree _inputTree;
@@ -16,11 +12,8 @@ public abstract class BaseApplication
 
     public virtual void Run()
     {
-        CreateGame();
         CreateInputModule();
         CreateScreenManager();
-
-        _game.Start();
     }
 
     public virtual void Update()
@@ -28,19 +21,17 @@ public abstract class BaseApplication
         _inputManager.Update();
         _screenManager.Update();
     }
-
-    private void CreateGame()
-    {
-        _game = new Model();
-        _game.Init(BOARD_SIZE);
-    }
-
-    private void CreateInputModule()
+    
+    protected void CreateInputModule()
     {
         _inputManager = new InputManager(CreateInputSource());
         _uiInputRoot = new UiInputRoot();
         _inputTree = new InputTree(_inputManager);
-        _inputTree.Add(_game);
+        AddInputHandlers();
+    }
+
+    protected virtual void AddInputHandlers()
+    {
         _inputTree.Add(_uiInputRoot);
     }
 
